5b15d77df89be76ed24a61a708c066c29e24c672
Willi Roquel <infowilliro@gmail.com>
2024-05-23 21:28:31 -0600

    agregando archivos estaticos con exito :)

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,13 +1,21 @@
 let minusBtn = document.getElementById('minus');
-let plusBtn = document.getElementById('plus');
-let cantidadInput = document.getElementById('cantidad');
-let cantidad = 1;
+  let plusBtn = document.getElementById('plus');
+  let cantidadInput = document.getElementById('cantidad');
+  let cantidad = 1;
 
-console.log('El archivo JavaScript se ha cargado correctamente.');
+  console.log('El archivo JavaScript se ha cargado correctamente.');
 
-function plus() {
-    cantidad += 1;
+  function plus() {
+    cantidad+= 1;
     cantidadInput.value = cantidad;
-}
+  }
 
- 
+  function minus() {
+    if (cantidad > 1) {
+      cantidad -= 1;
+      cantidadInput.value = cantidad;
+    }
+  }
+
+  minusBtn.addEventListener('click', minus);
+  plusBtn.addEventListener('click', plus);
